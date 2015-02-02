8880688bc5ccbe0143c3c68687d62cfc38364e22
Aron Fyodor Asor <aronasorman@gmail.com>
2015-02-01 23:59:49 -0800

    get the milestone and label difference

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"os"
+	"strings"
 )
 
 type Token string
@@ -33,14 +34,64 @@ func setupClient() *github.Client {
 	return github.NewClient(client)
 }
 
+func getLabelsToAdd(client *github.Client, currentLabels, expectedLabels []github.Label) (labelsToAdd []github.Label) {
+	labelsToAdd = make([]github.Label, 0) // have to make this 0 or else the first n elements are nil
+
+	// turn  expectedLabels into a hash "set", for easy searching
+	currentlabelset := make(map[string]bool)
+	for _, l := range currentLabels {
+		currentlabelset[l.String()] = false
+	}
+
+	for _, exlabel := range expectedLabels {
+		if _, ok := currentlabelset[exlabel.String()]; !ok {
+			labelsToAdd = append(labelsToAdd, exlabel)
+		}
+	}
+
+	return
+}
+
+func getMilestonesToAdd(client *github.Client, currentMilestones, expectedMilestones []github.Milestone) (milestonesToAdd []github.Milestone) {
+	milestonesToAdd = make([]github.Milestone, 0) // have to make this 0 or else the first n elements are nil
+
+	// turn  expectedLabels into a hash "set", for easy searching
+	currentmilestoneset := make(map[string]bool)
+	for _, l := range currentMilestones {
+		currentmilestoneset[l.String()] = false
+	}
+
+	for _, exlabel := range expectedMilestones {
+		if _, ok := currentmilestoneset[exlabel.String()]; !ok {
+			milestonesToAdd = append(milestonesToAdd, exlabel)
+		}
+	}
+
+	return
+}
+
 func main() {
 	var conf Config
-	filecontents, err := ioutil.ReadFile("config.yml.example")
+	filecontents, err := ioutil.ReadFile("config.yml")
 	if err != nil {
 		panic(err)
 	}
 	yaml.Unmarshal(filecontents, &conf)
-	fmt.Printf("%#v\n", &conf)
 
 	// retrieve information about the repos we're gonna manage
+	client := setupClient()
+
+	for _, repo := range conf.Repos {
+		spl := strings.Split(repo, "/")
+		owner := spl[0]
+		reponame := spl[1]
+
+		currentlabels, _, err := client.Issues.ListLabels(owner, reponame, nil)
+		if err != nil {
+			panic(err)
+		}
+
+		labelstoadd := getLabelsToAdd(client, currentlabels, conf.Labels)
+		fmt.Printf("% v\n", labelstoadd)
+	}
 }
